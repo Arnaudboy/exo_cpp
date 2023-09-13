ddec7d16d7ac74ee4442aba5b0e8a40712596fa6
msbenthaier <mohammed.ben-thaier@exalt-company.com>
2023-09-13 11:37:57 +0200

    Measuring elapsed time during record

--- recorder.cpp
+++ recorder.cpp
@@ -18,15 +18,21 @@ void generateNumbers(std::queue<int> & numbers, std::condition_variable & cv, st
         std::string value;
         std::cout << "Input temperature: ";
         getline(std::cin, value);
-        int val = 0;
-        try {
-            val = std::stoi(value);
-            value.clear();
-            std::cout << "Understood value: " << val << std::endl;
-            numbers.push(val);
-            producer_count++;
-        } catch (std::invalid_argument e) {
-            std::cerr << e.what();
+        if (value == "stop") {
+            workdone = true;
+            cv.notify_one();
+            break;
+        } else {
+            int val = 0;
+            try {
+                val = std::stoi(value);
+                value.clear();
+                std::cout << "Understood value: " << val << std::endl;
+                numbers.push(val);
+                producer_count++;
+            } catch (std::invalid_argument e) {
+                std::cerr << e.what();
+            }
         }
         cv.notify_one(); // Notify worker
         cv.wait(lk); // Wait for worker to complete
@@ -35,25 +41,28 @@ void generateNumbers(std::queue<int> & numbers, std::condition_variable & cv, st
 
 void work(std::queue<int> & numbers, std::condition_variable & cv, std::mutex & m, std::atomic<bool> & workdone)
 {
+    std::chrono::time_point<std::chrono::system_clock> start;
+    bool bStarted = false;
     while(!workdone.load())
     {
         std::unique_lock<std::mutex> lk(m);
         if (numbers.size() > 0) {
             std::cout << "Received: " << numbers.front() << std::endl;
+            if (!bStarted) {
+                start = std::chrono::system_clock::now();
+                bStarted = true;
+            } else {
+                auto current = std::chrono::system_clock::now();
+                auto current_time = std::chrono::duration_cast<std::chrono::seconds>(current - start).count();
+                std::cout << "Time since start: " << current_time << "(s)" << std::endl;
+            }
             numbers.pop();
             consumer_count++;
-            if (consumer_count >= 4) {
-                std::cout << "That's enough" << std::endl;
-                workdone.store(true);
-                cv.notify_one();
-                break;
-            }
         }
         cv.notify_one(); // Notify generator (placed here to avoid waiting for the lock)
         cv.wait(lk); // Wait for the generator to complete
      }
 }
-
 
 void record_and_save () {
     std::condition_variable cv;
@@ -65,16 +74,10 @@ void record_and_save () {
     std::thread producer(generateNumbers, std::ref(numbers), std::ref(cv), std::ref(m), std::ref(workdone));
     std::thread consumer(work, std::ref(numbers), std::ref(cv), std::ref(m), std::ref(workdone));
 
-
-    //wait for 3 seconds, then join the threads
-    std::this_thread::sleep_for(std::chrono::seconds(4));
-    // workdone = true;
-    // cv.notify_all(); // To prevent dead-lock
-
     producer.join();
     consumer.join();
 
     //output the counters
-    std::cout << producer_count << std::endl;
-    std::cout << consumer_count << std::endl;
+    std::cout << "Produced " << producer_count << " measurements" << std::endl;
+    std::cout << "Consumed " << consumer_count << " measurements" << std::endl;
 }
